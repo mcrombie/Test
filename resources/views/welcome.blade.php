2edabff3ba6eeb925885232b71e3b6a96832cfeb
Michael Crombie <mdcrombie@email.wm.edu>
2020-11-27 18:37:06 -0500

    Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,10 +89,7 @@
                     <a href="https://laracasts.com">Quercus</a>
                     <a href="https://laravel-news.com">Ilix</a>
                     <a href="https://blog.laravel.com">Acer</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://blog.laravel.com">Pinus</a>
                 </div>
             </div>
         </div>
